a351fb6384dbf700a2dc1b1654f8b7a6058a2c16
KimTaeHyeong <xogud3624@gmail.com>
2024-08-11 13:55:32 +0900

    스택

--- Programmers/Level2/Correct parentheses.cpp
+++ Programmers/Level2/Correct parentheses.cpp
@@ -1,12 +1,25 @@
 #include <string>
 #include <iostream>
+#include <stack>
 
 using namespace std;
 
 bool solution(string s)
 {
     bool answer = true;
+    stack<char> stk;
+    for (char element : s)
+    {
+        if (element == '(')
+            stk.push(element);
+        else if (element == ')' && !stk.empty())
+            stk.pop();
+        else
+            return false;
+    }
 
+    if (!stk.empty())
+        answer = false;
 
     return answer;
 }
