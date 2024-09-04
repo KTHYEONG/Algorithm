2d9abdbd9c1de592b417c35aab5302c44ec0031e
KimTaeHyeong <xogud3624@gmail.com>
2024-09-04 13:47:13 +0900

    LRU, vector

---
+++ Programmers/Level2/Cache1.cpp
@@ -0,0 +1,19 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int cacheSize, vector<string> cities)
+{
+    int answer = 0;
+
+    vector<string> v;
+    for (int i = 0; i < cities.size(); i++)
+    {
+        if (v.size() > cacheSize) {
+            v.p
+        }
+    }
+
+    return answer;
+}
