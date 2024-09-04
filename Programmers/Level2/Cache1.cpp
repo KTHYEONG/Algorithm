a2c84053878c57256da2affa396d2f4da23f9624
KimTaeHyeong <xogud3624@gmail.com>
2024-09-04 13:47:50 +0900

    LRU, vector

--- Programmers/Level2/Cache1.cpp
+++ Programmers/Level2/Cache1.cpp
@@ -1,5 +1,6 @@
 #include <string>
 #include <vector>
+#include <algorithm>
 
 using namespace std;
 
@@ -7,12 +8,29 @@ int solution(int cacheSize, vector<string> cities)
 {
     int answer = 0;
 
+    if (cacheSize == 0)
+        return cities.size() * 5;
+
     vector<string> v;
     for (int i = 0; i < cities.size(); i++)
     {
-        if (v.size() > cacheSize) {
-            v.p
+        transform(cities[i].begin(), cities[i].end(), cities[i].begin(), ::tolower);
+        auto it = find(v.begin(), v.end(), cities[i]);
+
+        if (it == v.end())
+        {
+            if (v.size() == cacheSize)
+                v.erase(v.begin());
+
+            answer += 5;
         }
+        else
+        {
+            answer++;
+            v.erase(it);
+        }
+
+        v.push_back(cities[i]);
     }
 
     return answer;
