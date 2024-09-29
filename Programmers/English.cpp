04110deaa42b0c04527a7df2335cb9cdf514cdc9
KimTaeHyeong <xogud3624@gmail.com>
2024-09-29 13:06:58 +0900

    English

--- Programmers/English.cpp
+++ Programmers/English.cpp
@@ -9,6 +9,8 @@ int main()
     unordered_map<string, string> vocabulary;
 
     vocabulary["statement"] = "진술 / 서술 / 성명";
+    vocabulary["motivate"] = "동기를 부여하다 / 이유(원인)가 되다";
+    vocabulary["strengths and weaknesses / merits and demerits"] = "장단점";
 
     for (const auto &entry : vocabulary)
     {
