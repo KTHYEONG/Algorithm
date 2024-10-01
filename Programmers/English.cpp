5ec9b215f40225b49a5f2505d0b0774521fd04c5
KimTaeHyeong <xogud3624@gmail.com>
2024-10-01 13:18:46 +0900

    English

--- Programmers/English.cpp
+++ Programmers/English.cpp
@@ -11,6 +11,7 @@ int main()
     vocabulary["statement"] = "진술 / 서술 / 성명";
     vocabulary["motivate"] = "동기를 부여하다 / 이유(원인)가 되다";
     vocabulary["strengths and weaknesses / merits and demerits"] = "장단점";
+    vocabulary["embarrassed"] = "당황하다";
 
     for (const auto &entry : vocabulary)
     {
