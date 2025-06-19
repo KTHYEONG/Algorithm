f70e35c6dc974450b75c960bf9793bd7363fb354
KimTaeHyeong <xogud3624@gmail.com>
2025-06-19 15:42:33 +0900

    구현/25-06-3

---
+++ 1783.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+
+using namespace std;
+
+// Sick Knight
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+	cout.tie(0);
+
+	int n, m;
+	cin >> n >> m;
+
+	if (n == 1)
+	{
+		cout << 1;
+	}
+	else if (n == 2)
+	{
+		cout << min(4, (m + 1) / 2);
+	}
+	else if (m < 7)
+	{
+		cout << min(4, m);
+	}
+	else
+	{
+		cout << m - 2;
+	}
+
+	return 0;
+}
