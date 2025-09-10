5c3c7364b553088b29111ca6f4d8646f70a6a538
xogud <xogud3624@gmail.com>
2025-09-10 12:49:40 +0900

    dp/25-09-2

---
+++ 17175.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int n;
+int dp[51];
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+	cout.tie(0);
+
+	cin >> n;
+
+	dp[0] = dp[1] = 1;
+	dp[2] = 3;
+	for (int i = 3; i <= n; i++)
+	{
+		dp[i] = (dp[i - 1] + dp[i - 2] + 1) % 1000000007;
+	}
+
+	cout << dp[n];
+
+	return 0;
+}
